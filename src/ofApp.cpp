241873793edc5e3a44daeeb5a1db29d85695876b
Darren Mothersele <darren@Virus2.local>
2015-01-27 21:51:20 +0000

    initial commit

---
+++ src/ofApp.cpp
@@ -0,0 +1,107 @@
+#include "ofApp.h"
+
+//--------------------------------------------------------------
+void ofApp::setup(){
+    
+    camWidth 		= 320;	// try to grab at this size.
+    camHeight 		= 240;
+    
+    //we can now get back a list of devices.
+    vector<ofVideoDevice> devices = vidGrabber.listDevices();
+    
+    for(int i = 0; i < devices.size(); i++){
+        cout << devices[i].id << ": " << devices[i].deviceName;
+        if( devices[i].bAvailable ){
+            cout << endl;
+        }else{
+            cout << " - unavailable " << endl;
+        }
+    }
+    
+    vidGrabber.setDeviceID(0);
+    vidGrabber.setDesiredFrameRate(60);
+    vidGrabber.initGrabber(camWidth,camHeight);
+    
+    videoTexture.allocate(camWidth,camHeight, GL_RGB);
+    ofSetVerticalSync(true);
+    
+    
+    finder.setup("haarcascade_frontalface_default.xml");
+    
+    
+}
+
+//--------------------------------------------------------------
+void ofApp::update(){
+    
+    vidGrabber.update();
+    if (vidGrabber.isFrameNew()) {
+        
+        img.setFromPixels(vidGrabber.getPixelsRef());
+        
+        //img.setFromPixels(vidGrabber.getPixels(), 320,240);
+
+        
+        finder.findHaarObjects(img);
+        
+        
+    }
+    
+
+}
+
+//--------------------------------------------------------------
+void ofApp::draw(){
+    //vidGrabber.draw(20,20);
+    img.draw(0,0);
+    
+    for(unsigned int i = 0; i < finder.blobs.size(); i++) {
+        ofRectangle cur = finder.blobs[i].boundingRect;
+        ofRect(cur.x, cur.y, cur.width, cur.height);
+    }
+}
+
+//--------------------------------------------------------------
+void ofApp::keyPressed(int key){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::keyReleased(int key){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseMoved(int x, int y ){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseDragged(int x, int y, int button){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mousePressed(int x, int y, int button){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseReleased(int x, int y, int button){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::windowResized(int w, int h){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::gotMessage(ofMessage msg){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::dragEvent(ofDragInfo dragInfo){ 
+
+}
