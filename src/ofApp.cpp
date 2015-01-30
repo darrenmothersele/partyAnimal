0e177d9802d09fdee06529d0888f4a9ef9de3070
alex.collins <alex.collins@gamesys.co.uk>
2015-01-30 14:44:10 +0000

    smooth face

--- src/ofApp.cpp
+++ src/ofApp.cpp
@@ -34,40 +34,67 @@ void ofApp::setup(){
     // setup face finder with settings from xml file
     finder.setup("haarcascade_frontalface_default.xml");
     
-    // grab first frame
-    img.setFromPixels(vidGrabber.getPixelsRef());
-    img.mirror(flipVert, flipHoriz);
-    finder.findHaarObjects(img);
-    
     // load mask image
-    mask.loadImage("mask2.png");
-    
+    mask.loadImage("mask0.png");
+   
 }
 
 //--------------------------------------------------------------
 void ofApp::update(){
     vidGrabber.update();
     if (vidGrabber.isFrameNew()) {
+        
         // if video grabber has a new frame
         // grab the image and run face finder
         img.setFromPixels(vidGrabber.getPixelsRef());
         img.mirror(flipVert, flipHoriz);
-        finder.findHaarObjects(img);
     }
 }
+
+const int maxFaces = 5;
+const int smoothingMax = 4;
+bool sampleExists[maxFaces][smoothingMax];
+ofRectangle smoothingSamples[maxFaces][smoothingMax];
+int smoothingIndex = 0;
 
 //--------------------------------------------------------------
 void ofApp::draw(){
     
+    finder.findHaarObjects(img, 50, 50);
+    
     // draw image to fit the whole screen
     img.draw(0, 0, ofGetWidth(), ofGetHeight());
     
-    for(unsigned int i = 0; i < finder.blobs.size(); i++) {
-
-        ofRectangle cur = finder.blobs[i].boundingRect;
-        mask.draw(ofMap(cur.x, 0, camWidth, 0, ofGetWidth()), ofMap(cur.y, 0, camHeight, 0, ofGetHeight()),
-                  ofMap(cur.width, 0, camWidth, 0, ofGetWidth()), ofMap(cur.height, 0, camHeight, 0, ofGetHeight()));
+    for(unsigned int faceIndex = 0; faceIndex < finder.blobs.size() && faceIndex < maxFaces; faceIndex++) {
+        
+        smoothingSamples[faceIndex][smoothingIndex] = finder.blobs[faceIndex].boundingRect;
+        sampleExists[faceIndex][smoothingIndex] = true;
+        
+        int x = 0, y = 0, w = 0, h = 0, n = 0;
+        
+        for (int i = 0; i < smoothingMax; i++) {
+            if (sampleExists[i]) {
+                ofRectangle cur = smoothingSamples[faceIndex][i];
+                x = x + cur.x;
+                y = y + cur.y;
+                w = w + cur.width;
+                h = h + cur.height;
+                n++;
+            }
+        }
+        
+        if (n > 0) {
+            x = x / n;
+            y = y / n;
+            w = w / n;
+            h = h / n;
+            
+            mask.draw(ofMap(x, 0, camWidth, 0, ofGetWidth()), ofMap(y, 0, camHeight, 0, ofGetHeight()),
+                  ofMap(w, 0, camWidth, 0, ofGetWidth()), ofMap(h, 0, camHeight, 0, ofGetHeight()));
+        }
     }
+    
+    smoothingIndex = (smoothingIndex + 1) % smoothingMax;
 }
 
 
